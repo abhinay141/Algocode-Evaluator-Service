62eb57ed9d8a2dda19c5d2cacdadc7d5f2d5b97a
abhinay141 <wayneabhi123@gmail.com>
2025-03-21 11:26:52 +0530

    basic express with typescript setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import serverConfig from './config/serverConfig';
+
+const app = express();
+
+app.listen(serverConfig.port, () => {
+
+    console.log(`Server is running on port ${serverConfig.port}`);
+});
